30144e803eae30bbcdb817adc8096ef3dfc85ee8
no-one <none>
2022-11-27 08:56:57 +0545

    2022-11-27

---
+++ jojotry.sql
@@ -0,0 +1,17 @@
+
+use bit3rdsem
+create table student
+(
+ id int not null primary key identity(1,1),
+ name nvarchar(255) not null,
+ address nvarchar(25) not null,
+ contact nvarchar(25) not null,
+ 
+ )
+ alter table student add phone int not null 
+ insert student( name,address,contact,phone)values ('samira','kalani','ss@gmail.com',5400)
+ select * from student
+ delete from student where id=8 
+ update student set name = 'sudarshan' where name ='suddu'
+
+ 
