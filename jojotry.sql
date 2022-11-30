b70467a2edfaa13e492056c0a6cc17f83ffc1939
no-one <none>
2022-11-30 08:26:52 +0545

    2022-11-30

--- jojotry.sql
+++ jojotry.sql
@@ -11,7 +11,7 @@ create table student
  alter table student add phone int not null 
  insert student( name,address,contact,phone)values ('samira','kalani','ss@gmail.com',5400)
  select * from student
- delete from student where id=8 
+ delete from student where name='chor' 
  update student set name = 'sudarshan' where name ='suddu'
  update student set name ='chor' where name ='SANDESH'
 
