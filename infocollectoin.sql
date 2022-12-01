8a49f963bb059fa7bc0a06a03aad5d0d53e08ea9
Aman Dangol <amandangol423@gmail.com>
2022-12-01 22:07:50 +0545

    2022-12-01

--- infocollectoin.sql
+++ infocollectoin.sql
@@ -5,6 +5,7 @@ create table details(
 	name nvarchar(255) not null,
 	age int not null,
  )
- insert into details(name,age,contact) values ('aman','21','9860112618')
+ insert into details(name,age,contact) values ('eren','21','911')
  alter table details add contact nvarchar(15);
  select * from details
+
