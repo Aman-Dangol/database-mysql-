f02165e702827067fd532baa3011ddce65225278
no-one <none>
2022-12-01 09:13:50 +0545

    2022-12-1

--- infocollectoin.sql
+++ infocollectoin.sql
@@ -5,6 +5,6 @@ create table details(
 	name nvarchar(255) not null,
 	age int not null,
  )
- insert into details(name,age,contact) values ('alina','25','98400')
+ insert into details(name,age,contact) values ('aman','21','9860112618')
  alter table details add contact nvarchar(15);
  select * from details
